ecacd18c6a86fb34d0a7506d6655f5fe8090f1b2
harutiro <harutiro2027@outlook.jp>
2022-08-12 17:53:36 +0900

    update:標準ライブラリから写真を取得した

--- app/src/main/java/net/harutiro/albumuriget/MainActivity.kt
+++ app/src/main/java/net/harutiro/albumuriget/MainActivity.kt
@@ -1,11 +1,28 @@
 package net.harutiro.albumuriget
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.OpenDocument()) {
+        findViewById<ImageView>(R.id.imageView).setImageURI(it)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            launcher.launch(arrayOf("image/*"))
+        }
     }
 }
